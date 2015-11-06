7ba9bc660e94a1a92e6e026267c9a32d3976b75f
Stephen Paradis <sparadis@localhost.localdomain>
2015-11-06 17:24:12 -0500

    made restarter function and changed MAX_SIZE

--- mastermind.c
+++ mastermind.c
@@ -4,33 +4,34 @@
 #include <ctype.h>
 #include <string.h>
 
-#define MAX_SIZE 6
+const unsigned int MAX_SIZE = 6;
 
-void secret_guess(int count, char * secret_holder);
-int type_check(int count, char * array);
-void red_white_checker(int *red, int *white, int red_counter, int white_counter, char * computer_guess, char * guess);
+
+
+void secret_guess(int counter, char * secret_holder);
+int type_check(int counter, char * array);
+void red_white_checker(int *red, int *white, char * computer_guess, char * guess, int counter);
+int if_winner(int *red, int total_guesses);
+int restarter(int counter, char * computer_guess, char * guess);
 
 int main(void)
 {
-	char computer_guess[MAX_SIZE];
+	char *computer_guess;
 	char *guess;
-	char *re_try;
 	int red[] = {0};
 	int white[] = {0};
-	int red_counter = 0;
-	int white_counter = 0;
-	int count = 0;
+	int counter = (MAX_SIZE - 2);
 	int total_guesses = 0;
-	int c;
-	int begin_again;
+	int character_eater;
 
+	computer_guess = malloc(MAX_SIZE);
 	guess = malloc(MAX_SIZE);
-	
-	re_try = malloc(MAX_SIZE);
+
+	printf("It is %d\n", counter);
 
 	srand(time(NULL));
 
-	secret_guess(count, computer_guess);
+	secret_guess(counter, computer_guess);
 	
 	start:
 	{
@@ -39,7 +40,7 @@ int main(void)
 			printf("Guess a number (q to quit): ");
 			fgets(guess, MAX_SIZE, stdin);
 
-			if (strlen(guess) < 5)
+			if (strlen(guess) < (MAX_SIZE - 1))
 			{
 				if (guess[0] == 'q')
 				{
@@ -48,14 +49,14 @@ int main(void)
 				}
 				else
 				{
-					printf("Not enough digits. Try again.\n");
+					printf("Invalid input. Try again.\n");
 					goto start;
 				}
 			}
 			else if (guess[4] != '\n')
 			{
-				printf("To many digits. Try again.\n");
-				while((c = getchar()) != '\n' && c != EOF)
+				printf("Invalid input. Try again.\n");
+				while((character_eater = getchar()) != '\n' && character_eater != EOF)
 				{
 					//eats rest of input in stdin buffer
 				}
@@ -63,57 +64,44 @@ int main(void)
 			}
 			else
 			{
-				if (!(type_check(count, guess)))
+				if (!(type_check(counter, guess)))
 				{
 					printf("Type was incorrect. Try again.\n");
 					goto start;
 				}
 			}
-			red_white_checker(red, white, red_counter, white_counter, computer_guess, guess);
+
+			red_white_checker(red, white, computer_guess, guess, counter);
+
 			printf("R:%d and W:%d\n", *red, *white);
 			total_guesses++;
-			if (*red == 4)
+
+			if(0 == if_winner(red, total_guesses))
 			{
 
-				printf("You have won in %d guesses\n", total_guesses);
-				printf("Would you like to play again? y or n: ");
-				fgets(re_try, sizeof(re_try), stdin);
-
-
-				if (re_try[0] == 'y')
+				if(1 == restarter(counter, computer_guess, guess))
 				{
-					begin_again = 1;
-					secret_guess(count, computer_guess);
 					*red = 0;
 					*white = 0;
-					goto start;	
-				}				
-				else if (re_try[0] == 'n')
-				{
-					begin_again = 0;
-					break;
+					total_guesses = 0;
+					goto start;
 				}
 				else
 				{
-					do {
-						printf("I did not understand that.\n");
-					} while (begin_again != 1 && begin_again != 0);
+					break;
 				}
-	
 			}
 
 			*red = 0;  //resetting value of red for next guess
 			*white = 0;  //resetting the value of white for next guess
 
 		}
-		free(guess);
-		free(re_try);
 	}
 }
 
-void secret_guess(int count, char * secret_holder)
+void secret_guess(int counter, char * secret_holder)
 {
-	for (count = 0; count < 4; count++)
+	for (int count = 0; count < counter; count++)
 	{
 		int r = (rand() % 10) + '0';
 		secret_holder[count] = r;
@@ -121,9 +109,9 @@ void secret_guess(int count, char * secret_holder)
 	}
 }
 
-int type_check(int count, char * array)
+int type_check(int counter, char * array)
 {
-	for (count = 0; count < 4; count++)
+	for (int count = 0; count < counter; count++)
 	{
 		if (!isdigit(array[count]))
 		{
@@ -141,10 +129,10 @@ int type_check(int count, char * array)
 	return 1;
 }
 
-void red_white_checker(int *red, int *white, int red_counter, int white_counter, char * computer_guess, char * guess)
+void red_white_checker(int *red, int *white, char * computer_guess, char * guess, int counter)
 {
 
-	for (red_counter = 0; red_counter < 4; red_counter++)
+	for (int red_counter = 0; red_counter < counter; red_counter++)
 	{
 		if (computer_guess[red_counter] == guess[red_counter])
 		{
@@ -153,7 +141,7 @@ void red_white_checker(int *red, int *white, int red_counter, int white_counter,
 		}
 		else
 		{
-			for (white_counter = 0; white_counter < 4; white_counter++)
+			for (int white_counter = 0; white_counter < counter; white_counter++)
 			{
 				if (computer_guess[red_counter] == guess[white_counter])
 				{
@@ -164,4 +152,48 @@ void red_white_checker(int *red, int *white, int red_counter, int white_counter,
 			}
 		}
 	}
+}
+
+int if_winner(int *red, int total_guesses)
+{
+	if (*red == 4)
+	{
+		printf("You have won in %d guesses\n", total_guesses);
+		return 0;
+	}
+	else
+	{
+		return 1;
+	}
+}
+
+int restarter(int counter, char * computer_guess, char * guess)
+{
+	char *re_try;
+
+	re_try = malloc(MAX_SIZE);
+
+	while(1)
+	{
+		printf("Would you like to play again? y or n: ");
+		fgets(re_try, MAX_SIZE, stdin);
+
+
+		if (re_try[0] == 'y')
+		{
+			secret_guess(counter, computer_guess);
+			free(re_try);
+			return 1;	
+		}				
+		else if (re_try[0] == 'n')
+		{
+			free(re_try);
+			free(guess);
+			free(computer_guess);
+			exit(1);
+		}
+		else
+			printf("I did not understand. Try again.\n");
+	}
+	return 0;
 }
